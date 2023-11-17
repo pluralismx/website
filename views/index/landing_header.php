78e18c02cc73fcdeaa7c78c394b26370e034586c
Gerardo <programador3@grupovalcas.mx>
2023-11-17 10:13:08 -0800

    animacion del banner

--- views/index/landing_header.php
+++ views/index/landing_header.php
@@ -16,25 +16,26 @@
                 "Excelencia digital",
                 "Captura, Crea, Cautiva",
                 "Experiencias Digitales",
-                "Servicio en putiza"
+                "Desarrollo web integral"
             ]
             var phrases_qty = phrases.length;
             document.querySelector('#banner-phrase').innerHTML = phrases[loop];
 
-            setInterval( () => {
+            function bannerAnimation() {
                 document.querySelector('#banner-phrase').innerHTML = phrases[loop];
                 document.querySelector('#banner-phrase').style.animationName = "headerSlideIn";
                 document.querySelector('#banner-phrase').style.animationDuration = "4s"; // Adjust the duration as needed
                 document.querySelector('#banner-phrase').style.animationIterationCount = "infinite";
                 
-
-
                 loop++;
                 if(loop > phrases_qty - 1){
                     loop = 0;
                 }
+            }
 
-            }, 4000); 
+            bannerAnimation();
+
+            setInterval(bannerAnimation, 4000); 
 
         });
     </script>
@@ -43,11 +44,44 @@
     <!-- Mobile header -->
     <section class="mobile-section-header">
         <div class="mobile-container-section-header">
-            <h1>Web apps</h1>
-            <h2>Desarrollo web que funciona,</h2>
-            <h2>Desarrollo web para ganar</h2>
+            <!-- Banner -->
+            <h1 id="mobile-banner-phrase"></h1>
+            <!-- Static text -->
+            <h2>De la Visión a la Realidad</h2>
+            <h2>Tu Solucion Web Integral</h2>
         </div>
     </section>
+
+    <script>
+        document.addEventListener("DOMContentLoaded", function () {
+            var loop = 0;
+            var phrases = [
+                "Excelencia digital",
+                "Captura, Crea, Cautiva",
+                "Experiencias Digitales",
+                "Desarrollo web integral"
+            ]
+            var phrases_qty = phrases.length;
+            document.querySelector('#mobile-banner-phrase').innerHTML = phrases[loop];
+
+            function mobileBannerAnimation() {
+                document.querySelector('#mobile-banner-phrase').innerHTML = phrases[loop];
+                document.querySelector('#mobile-banner-phrase').style.animationName = "headerSlideIn";
+                document.querySelector('#mobile-banner-phrase').style.animationDuration = "4s"; // Adjust the duration as needed
+                document.querySelector('#mobile-banner-phrase').style.animationIterationCount = "infinite";
+                
+                loop++;
+                if(loop > phrases_qty - 1){
+                    loop = 0;
+                }
+            }
+
+            mobileBannerAnimation();
+
+            setInterval(mobileBannerAnimation, 4000); 
+
+        });
+    </script>
 
 
 
